1e256267f340f95383b9d074440e65b9de56f270
WADE <MEANTOINE12@GMAIL.COM>
2025-05-08 18:16:27 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    fib = [0,1]
+
+    while len(fib) != length:
+        next_number = fib[-1] + fib[-2]
+        fib.append(next_number)
+
+    print(fib)
+    
+
+print_fibonacci(10)
